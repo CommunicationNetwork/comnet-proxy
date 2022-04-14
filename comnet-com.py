af7658130f25f534acb53ece3905b8f21a40bbcd
myyxl <22593897+myyxl@users.noreply.github.com>
2022-04-14 11:54:01 +0200

    initial commit

---
+++ comnet-com.py
@@ -0,0 +1,37 @@
+import socket
+from unicom.unicom_receiver import UnicomReceiver
+
+backend_connected = False
+
+class ProxyServer:
+    def __init__(self, host, port, receiver):
+        self.socket = socket.socket()
+        self.socket.bind((host, port))
+        self.receiver = receiver
+
+    def start(self):
+        global backend_connected
+        self.socket.listen()
+        while True:
+            backend, _ = self.socket.accept()
+            backend_connected = True
+            self.receiver.backend = backend
+            self.receiver.start()
+            while backend_connected:
+                continue
+            self.receiver.stop()
+
+def send_callback(data, backend):
+    global backend_connected
+    try:
+        backend.sendall(data)
+    except:
+        backend_connected = False
+        
+def main():
+    receiver = UnicomReceiver("wlp4s0mon", "receiver", send_callback)
+    proxy = ProxyServer("localhost", 5000, receiver)
+    proxy.start()
+
+if __name__ == "__main__":
+    main()
---
+++ unicom/unicom_receiver.py
@@ -0,0 +1,28 @@
+from scapy.all import *
+
+class UnicomReceiver:
+    def __init__(self, mon_interface, receiver_name, receive_callback):
+        self.receiver_id = self.generate_id(receiver_name)
+        self.receive_callback = receive_callback
+        self.sniffer = AsyncSniffer(iface=mon_interface, prn=self.internal_callback)
+        self.backend = None
+
+    def internal_callback(self, frame):
+        if frame.haslayer(Dot11) and frame.type == 2 and frame.subtype == 5:
+            if frame.addr2.replace(":", "").startswith(self.receiver_id):
+                data = bytes(frame[Dot11].payload)
+                self.receive_callback(data, self.backend)
+            
+    def start(self):
+        self.sniffer.start()
+
+    def stop(self):
+        self.sniffer.stop()
+
+
+    # https://gist.github.com/mengzhuo/180cd6be8ba9e2743753
+    def generate_id(self, s):                                                                                                                                
+        hash = 5381
+        for x in s:
+            hash = (( hash << 5) + hash) + ord(x)
+        return hex(hash & 0xFFFFFFFF)[2:8]
